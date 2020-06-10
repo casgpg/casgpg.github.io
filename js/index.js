f762a5211ee124a518c3a57a090515df8bb8046a
Himanshu Gupta <himanshugupta.396@gmail.com>
2020-06-10 18:57:32 +0530

    index.js refactored to old index.js

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ jQuery.fn.liScroll = function(settings) {
 	settings = jQuery.extend({
 		travelocity: 0.03
 		}, settings);		
-		return $(this).each(function(){
+		return this.each(function(){
 				var $strip = jQuery(this);
 				$strip.addClass("newsticker")
 				var stripHeight = 0;
@@ -46,18 +46,18 @@ jQuery.fn.liScroll = function(settings) {
 				function scrollnews(spazio, tempo){
 				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
 				}
-				// scrollnews(totalTravel, defTiming);				
+				scrollnews(totalTravel, defTiming);				
 				$strip.hover(function(){
-				  jQuery(this).stop();
-  				},
-  				function(){
-    				var offset = jQuery(this).offset();
-    				var residualSpace = stripHeight;
-    				var residualTime = residualSpace/settings.travelocity;
-    				scrollnews(residualSpace, residualTime);
+				jQuery(this).stop();
+				},
+				function(){
+				var offset = jQuery(this).offset();
+				var residualSpace = offset.top + stripHeight;
+				var residualTime = residualSpace/settings.travelocity;
+				scrollnews(residualSpace, residualTime);
 				});			
 		});	
 };
-  $(document).ready(function() {
-        $("ul#ticker01").liScroll();
-    });
+$(function(){
+    $("ul#ticker01").liScroll();
+});
