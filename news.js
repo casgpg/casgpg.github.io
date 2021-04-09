4e83512a272fef151414d4e204488d7e9a0af78f
SGPG Associates <65170008+casgpg@users.noreply.github.com>
2021-04-09 22:23:06 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,14 +1,14 @@
 var news = [
 		['24.02.2020', 'HeadingHeadingHea dingHeadingHeading', 'https://google.com'],
 		['24.02.2020', 'Heading', 'https://google.com'],
+		['24.02.2020', 'Heading', 'https://google.com'],
+		['24.02.2020', 'Heading', 'https://google.com'],
 		['24.02.2020', 'HeadingHeadingHeadingHeading', 'https://google.com'],
 		['24.02.2020', 'HeadingHeadingHeading', 'https://google.com'],
 		['24.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
-		['24.02.2020', 'Heading', 'https://google.com'],
-		['24.02.2020', 'Heading', 'https://google.com'],
 ]
 
 // Example
