330b18120ddcbf488b33e29fcd37d1de556552a7
SGPG Associates <65170008+casgpg@users.noreply.github.com>
2021-05-01 13:00:00 +0530

    Update news.js

--- news.js
+++ news.js
@@ -9,7 +9,7 @@ var news = [
 		['24.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
-		['2.02.2020', 'Heading', 'https://google.com']
+		['2.02.2020', 'Heading', 'https://google.com'],
 ]               
 
 
