4a380371dab13eb1e23ef5e5559230c5245c9993
SGPG Associates <65170008+casgpg@users.noreply.github.com>
2021-05-01 13:03:33 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,7 +1,8 @@
 var news = [
+		['01.05.2021', 'Taxpayers registered under Companies Act, 2013, can now file their Form GSTR-1 and GSTR-3B with EVC also, apart from using DSC, on GST Portal', 'https://www.gst.gov.in/'],
 		['09.04.2021', 'Quarterly taxpayers are able to see the GSTR3B tile for Jan, Feb and Mar 21. They are requested to file GSTR3B for the quarter Jan-Mar, 21 using tile for Mar, 21', 
 		 'https://www.gst.gov.in/'],
-		['01.05.2021', 'Taxpayers registered under Companies Act, 2013, can now file their Form GSTR-1 and GSTR-3B with EVC also, apart from using DSC, on GST Portal', 'https://www.gst.gov.in/'],
+		['24.02.2020', 'Heading', 'https://google.com'],
 		['24.02.2020', 'Heading', 'https://google.com'],
 		['24.02.2020', 'Heading', 'https://google.com'],
 		['24.02.2020', 'HeadingHeadingHeadingHeading', 'https://google.com'],
