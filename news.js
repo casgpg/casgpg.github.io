dbe1be71bc32a17a97f6e11cb62596dd23006a3d
SGPG Associates <65170008+casgpg@users.noreply.github.com>
2021-09-09 21:28:09 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,4 +1,5 @@
 var news = [
+		['09.09.2021', 'CBDT extends due dates for filing of Income Tax Returns and various reports of audit for Assessment Year 2021-22', 'https://pib.gov.in/PressReleseDetail.aspx?PRID=1753603'],
 		['05.05.2021', 'Extension in dates of various GST Compliances for GST Taxpayers', 'https://www.gst.gov.in/newsandupdates/read/469'],
 		['01.05.2021', 'Taxpayers registered under Companies Act, 2013, can now file their Form GSTR-1 and GSTR-3B with EVC also, apart from using DSC, on GST Portal', 'https://www.gst.gov.in/'],
 		['09.04.2021', 'Quarterly taxpayers are able to see the GSTR3B tile for Jan, Feb and Mar 21. They are requested to file GSTR3B for the quarter Jan-Mar, 21 using tile for Mar, 21', 
