446a270b43a821ef21b8a3b130eadc9be6cf419e
Himanshu Gupta <himanshugupta.396@gmail.com>
2021-09-23 14:45:10 +0530

    closed the braces

--- news.js
+++ news.js
@@ -13,7 +13,8 @@ var news = [
 		['24.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
 		['2.02.2020', 'Heading', 'https://google.com'],
-		['2.02.2020', 'Heading', 'https://google.com'],              
+		['2.02.2020', 'Heading', 'https://google.com']
+]
 
 
 // Example
